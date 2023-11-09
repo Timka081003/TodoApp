118f43409cd3f3d226244599b43b1f4f5fc7ee33
Timka <77959253+Timka081003@users.noreply.github.com>
2023-11-09 20:27:55 +0600

    TodoApp

---
+++ app/src/main/java/com/example/todoapp/TodoAdapter.kt
@@ -0,0 +1,64 @@
+package com.example.todoapp
+
+import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class TodoAdapter(
+    private val todos: MutableList<Todo>
+) :RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+    class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        return TodoViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_todo,
+                parent,
+                false
+            )
+        )
+    }
+    fun addTodo(todo: Todo){
+        todos.add(todo)
+        notifyItemInserted(todos.size - 1)
+
+    }
+    fun deleteTodos(){
+        todos.removeAll{ todo ->
+            todo.isChecked
+        }
+        notifyDataSetChanged()
+    }
+
+    private fun toggleStrikeThrough(tvTodoTitle: TextView, isCecked: Boolean){
+        if (isCecked){
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags or STRIKE_THRU_TEXT_FLAG
+        }else{
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+        }
+    }
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val curTodo = todos[position]
+        holder.itemView.apply {
+            val tvToDoTitle : TextView = findViewById(R.id.tvTodoTitle)
+            val cbDone: CheckBox = this.findViewById(R.id.cbDone)
+            tvToDoTitle.text = curTodo.title
+            cbDone.isChecked = curTodo.isChecked
+            toggleStrikeThrough(tvToDoTitle,curTodo.isChecked)
+            cbDone.setOnCheckedChangeListener { _, isChecked ->
+                toggleStrikeThrough(tvToDoTitle, isChecked)
+                curTodo.isChecked = !curTodo.isChecked
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
+}
